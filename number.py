82d1b65b60f2eee7066ca3fdb3a5beeeea51c1bd
kirill965 <volik_kirill@eclipso.eu>
2020-05-26 14:42:09 +0200

    Data types in Python: numbers

---
+++ number.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# Numbers in python are integers and float-point numbers
+
+int_numb = 1
+float_numb = 1.4
+
+#They converts automatically in follow situation
+
+summa = int_numb + float_numb
+print(summa)
